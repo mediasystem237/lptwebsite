281316dd62ceb06aad5dab40fe6c697de1d9e59a
mediasystem237 <contact@mediasystem.cm>
2024-09-22 03:52:47 +0100

    new update ico app anama

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="@yield('meta_description', 'Description par défaut de votre site')">
     <meta name="keywords" content="@yield('meta_keywords', 'mots-clés, par, défaut')">
-    <title>@yield('title', 'Nom de votre site')</title>
+    <title>{{ config('app.name', 'Laravel') }}</title>
     <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
 
 
